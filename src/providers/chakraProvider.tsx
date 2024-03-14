944121931ad0b5d781c2e3447d6710e93a2a90ba
Ryan Albuquerque <56835754+Ryan-Albuquerque@users.noreply.github.com>
2024-03-14 00:23:27 -0400

    setup

---
+++ src/providers/chakraProvider.tsx
@@ -0,0 +1,8 @@
+// app/providers.tsx
+'use client';
+
+import { ChakraProvider as Provider } from '@chakra-ui/react';
+
+export function ChakraProvider({ children }: { children: React.ReactNode }) {
+  return <Provider>{children}</Provider>;
+}
